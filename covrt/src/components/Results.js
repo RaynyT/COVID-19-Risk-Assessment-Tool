e56cd414fbb08d17167e2f262cfc752ed1bc2cc0
Chris Hogan <chrishogan17@outlook.com>
2021-04-21 19:35:04 -0700

    Change from list style to icon style

--- covrt/src/components/Results.js
+++ covrt/src/components/Results.js
@@ -1,7 +1,13 @@
 import riskMeterImage from '../images/risk-meter.svg'
+import houseIcon from '../images/house-vector.svg'
+import clockIcon from '../images/clock-vector.svg'
+import peopleIcon from '../images/people-icon-vector.svg'
+import rulerIcon from '../images/ruler-vector.svg'
+import volumeIcon from '../images/volume-vector.svg'
 
 import "./Results.css"
 import { Button } from 'reactstrap'
+
 
 export default function Results(props){
 
@@ -37,6 +43,27 @@ export default function Results(props){
         );
     }
 
+
+    // This is the stupidest hacky way to fix the text overflowing from the button
+    // But since this is the only string of text that does it, I'm using this for now
+    // until I have more time to fix the general problem
+    let volumeText = props.speakingVolume;
+    if (props.speakingVolume === "Speaking loudly or shouting") {
+        volumeText = "Speaking loudly";
+    }
+
+    // Another stupid edge case I felt like catching
+    let attendeesText = props.activityBasicInfo.attendees
+    console.log(attendeesText);
+    if (attendeesText >= 999999999) {
+        attendeesText = "100000000+";
+    }
+
+    let maskAmountText = Math.round(props.othersMask.percent / 100 * props.activityBasicInfo.attendees);
+    if (maskAmountText >= 999999999) {
+        maskAmountText = "100000000+";
+    }
+
     return (
         <div className="outer">
             <div className="results-main-container">
@@ -48,6 +75,23 @@ export default function Results(props){
                 <div className="horizontal-center">
                     <div>
                         <h3 className="list-title">Activity</h3>
+
+                        <div className="container">
+                            <div className="row img-card-row">
+                                <ImageCard image={houseIcon} desc={props.activityBasicInfo.setting} alt="Test" />
+                                <ImageCard image={clockIcon} desc={props.activityBasicInfo.hours + "h " + props.activityBasicInfo.minutes +"m"} alt="Clock icon" />
+                                <ImageCard image={peopleIcon} desc={attendeesText + " people"} alt="Icon of two people" />
+                                <ImageCard image={rulerIcon} desc={props.distancing} alt="Ruler icon" />
+                            </div>
+                            <div className="row img-card-row">
+                                <ImageCard image={volumeIcon} desc={volumeText} alt="Speaker icon" />
+                                <ImageCard noImage desc={"I will wear: " + props.ownMask} alt="none" />
+                                <ImageCard noImage desc={"Others wear: " + props.othersMask.type} alt="none" />
+                                <ImageCard noImage desc={maskAmountText + " people will wear masks"} alt="none" />
+                            </div>
+                        </div>
+
+                        {/*
                         <ul className="selection-list">
                             <li className="selection-list-item">Activity Environment: <span className="blue">{props.activityBasicInfo.setting}</span></li>
                             <li className="selection-list-item">Duration: <span className="blue">{props.activityBasicInfo.hours}h {props.activityBasicInfo.minutes}m</span></li>
@@ -58,6 +102,7 @@ export default function Results(props){
                             <li className="selection-list-item">Others' Mask: <span className="blue">{props.othersMask.type}</span></li>
                             <li className="selection-list-item">Number of Others Wearing Masks: <span className="blue">{Math.round(props.othersMask.percent / 100 * props.activityBasicInfo.attendees)}</span></li>
                         </ul>
+                        */}
                         <h3 className="list-title">Demographic</h3>
                         <ul className="selection-list">
                             <li className="selection-list-item">State: <span className="blue">{props.location.state}</span></li>
@@ -68,6 +113,28 @@ export default function Results(props){
                 </div>
                 <div className="horizontal-center">
                     <Button>How is my risk calculated?</Button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function ImageCard(props) {
+
+    let imageClass = "img-card-image-container"
+
+    if(props.noImage) {
+        imageClass = "display-none";
+    }
+
+    return(
+        <div className="img-card">
+            <div className="img-card-content">
+                <div className={imageClass}>
+                    <img className="img-card-image" src={props.image} alt={props.alt} />
+                </div>
+                <div className="img-card-text">
+                    {props.desc}
                 </div>
             </div>
         </div>
