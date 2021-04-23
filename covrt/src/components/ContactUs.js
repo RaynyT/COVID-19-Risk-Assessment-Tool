ce8e7dfb43080f8a908d7b79c335fee3dccd7ddc
Chris Hogan <chrishogan17@outlook.com>
2021-04-23 13:27:48 -0700

    Add links section

--- covrt/src/components/ContactUs.js
+++ covrt/src/components/ContactUs.js
@@ -1,11 +1,19 @@
 import './ContactUs.css';
+import { MailIcon } from '@primer/octicons-react'
+
 
 export default function ContactUs() {
 
     return (
         <div className="outer">
             <div className="contact-main-container">
-
+                <div className="contact-links-container">
+                    <h1 className="contact-heading">Contact Us</h1>
+                    <h2 className="contact-email">
+                        <MailIcon className="mail-icon"/>
+                        CovidAware@gmail.com
+                    </h2>
+                </div>
             </div>
         </div>
     );
