b4b3b337f081ab694de176ed88bedc44723ea694
Chris Hogan <chrishogan17@outlook.com>
2021-04-22 21:52:44 -0700

    Style dropdown and make it it's own component

--- covrt/src/components/InfoPage.js
+++ covrt/src/components/InfoPage.js
@@ -1,57 +1,51 @@
 import './InfoPage.css'
 import { Collapse, Card, CardBody} from 'reactstrap'
-import { ChevronDownIcon } from '@primer/octicons-react';
+import { ChevronDownIcon, ChevronUpIcon } from '@primer/octicons-react';
 import { useState } from 'react';
 
 export default function InfoPage() {
-
-    const [isOpenA, setIsOpenA] = useState(false);
-    const [isOpenB, setIsOpenB] = useState(false);
-    const [isOpenC, setIsOpenC] = useState(false);
-
-
-    const toggleA = () => setIsOpenA(!isOpenA);
-    const toggleB = () => setIsOpenB(!isOpenB);
-    const toggleC = () => setIsOpenC(!isOpenC);
-
 
     return (
         <div className="outer">
             <div className="info-main-container">
                 <h1 className="info-title">Frequently Asked Questions</h1>
                 <div>
-                    <div>
-                        <button className="btn btn-outline-secondary" onClick={toggleA}>Test A</button>
-                        <Collapse isOpen={isOpenA}>
-                            <Card>
-                                <CardBody>
-                                    Test A
-                                </CardBody>
-                            </Card>
-                        </Collapse>
-                    </div>
-                    <div>
-                        <button className="btn btn-outline-secondary" onClick={toggleB}>Test B</button>
-                        <Collapse isOpen={isOpenB}>
-                            <Card>
-                                <CardBody>
-                                    Test B
-                                </CardBody>
-                            </Card>
-                        </Collapse>
-                    </div>
-                    <div>
-                        <button className="btn btn-outline-secondary" onClick={toggleC}>Test C</button>
-                        <Collapse isOpen={isOpenC}>
-                            <Card>
-                                <CardBody>
-                                    Test C
-                                </CardBody>
-                            </Card>
-                        </Collapse>
-                    </div>
+                    <Dropdown title="What is CovidAware?" body="Test"/>
+                    <Dropdown title="What is CovidAware?" body="Test"/>
+                    <Dropdown title="What is CovidAware?" body="Test"/>
+                    <Dropdown title="What is CovidAware?" body="Test"/>
+                    <Dropdown title="What is CovidAware?" body="Test"/>
                 </div>
             </div>
+        </div>
+    )
+}
+
+function Dropdown(props) {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => {
+        setIsOpen(!isOpen);
+    }
+
+    return (
+        <div>
+            <button className="dropdown-btn" onClick={toggle}>
+                <div>{props.title}</div>
+                {isOpen
+                    ? <ChevronUpIcon />
+                    : <ChevronDownIcon />
+                }
+            </button>
+            <Collapse isOpen={isOpen}>
+                <Card>
+                    <CardBody>
+                        {props.body}
+                    </CardBody>
+                </Card>
+            </Collapse>
+            <hr className="info-hr" />
         </div>
     )
 }
