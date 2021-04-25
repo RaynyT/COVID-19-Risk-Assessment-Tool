0a5ffef6dc85336429d5a2254f364bc609d5020b
Chris Hogan <chrishogan17@outlook.com>
2021-04-24 20:59:32 -0700

    Add functional and styled contact form to page

--- covrt/src/components/ContactUs.js
+++ covrt/src/components/ContactUs.js
@@ -1,5 +1,10 @@
 import './ContactUs.css';
 import { MailIcon } from '@primer/octicons-react'
+import { useForm, ValidationError } from '@formspree/react';
+import { Link } from 'react-router-dom';
+
+
+import contactImage from '../images/contact-us.svg'
 
 
 export default function ContactUs() {
@@ -14,7 +19,66 @@ export default function ContactUs() {
                         CovidAware.ischool@gmail.com
                     </h2>
                 </div>
+                <div className="questions-container">
+                    <h1 className="questions-heading">Have questions?</h1>
+                    <p className="questions-text">Visit our <Link to="/about">FAQ </Link> page</p>
+                    <p className="questions-text">or fill out the form below!</p>
+                    <img className="questions-image" alt="Person with speech bubbles" src={contactImage}/>
+                </div>
+                <div className="email-form-container">
+                    <ContactForm />
+                </div>
             </div>
         </div>
     );
 }
+
+function ContactForm() {
+    const [state, handleSubmit] = useForm("xdoykrzz");
+    if (state.succeeded) {
+        return <h3 className="submission-response-text">Thanks for contacting us! We'll get back to you as soon as possible!</h3>;
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="form-group ">
+                <div className="horizontal-center">
+                    <input className="contact-input" id="name" type="text" name="name" aria-label="Name" placeholder="Name"/>
+                    <ValidationError
+                        prefix="Email"
+                        field="email"
+                        errors={state.errors}
+                    />
+                </div>
+                <div className="horizontal-center">
+                    <input className="contact-input" id="email" type="email" name="email" aria-label="Email" placeholder="Email"/>
+                    <ValidationError
+                        prefix="Email"
+                        field="email"
+                        errors={state.errors}
+                    />
+                </div>
+                <div className="horizontal-center">
+                    <div className="message-form">
+                        <label htmlFor="message" className="message-label">
+                            Message
+                        </label>                    
+                        <textarea className="message-input" id="message" name="message" aria-label="Message" />
+                        <ValidationError
+                            prefix="Message"
+                            field="message"
+                            errors={state.errors}
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="horizontal-center">
+                <div className="submit-container">
+                    <button className="btn btn-outline-primary" type="submit" disabled={state.submitting}>
+                        Submit
+                    </button>
+                </div>
+            </div>
+        </form>
+    );
+  }
+  
