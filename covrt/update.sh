b868ac1b37d984659ebd326973d898cb3bdd8b29
RaynyT <rjoytilley@gmail.com>
2021-05-06 04:53:12 -0700

    add some handlers

--- covrt/update.sh
+++ covrt/update.sh
@@ -1,12 +1,12 @@
-sudo docker pull covidaware/calculator
 sudo docker stop calculator
 sudo docker rm -f calculator
+sudo docker pull covidaware/calculator
 
 sudo -E docker run \
     -d \
     -p 443:443 \
     -p 80:80 \
     --name calculator \
-    --network=ra_net \
+    --network ra_net \
     -v /etc/letsencrypt:/etc/letsencrypt:ro \
     covidaware/calculator
---
+++ server/gateway/handlers/cors.go
@@ -0,0 +1,29 @@
+package handlers
+
+import "net/http"
+
+/*
+  Access-Control-Allow-Origin: *
+  Access-Control-Allow-Methods: GET, PUT, POST, PATCH, DELETE
+  Access-Control-Allow-Headers: Content-Type, Authorization
+  Access-Control-Expose-Headers: Authorization
+  Access-Control-Max-Age: 600
+*/
+
+// CORS struct
+type CORS struct {
+	Handler http.Handler
+}
+
+func (c *CORS) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, PUT, POST, PATCH, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	w.Header().Set("Access-Control-Expose-Headers", "Authorization")
+	w.Header().Set("Access-Control-Max-Age", "600")
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+	c.Handler.ServeHTTP(w, r)
+}
