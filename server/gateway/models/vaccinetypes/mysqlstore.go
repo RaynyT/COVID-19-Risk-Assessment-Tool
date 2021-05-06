532be65b07b15c8de3e2ffe48cd9e9f4aa24321d
RaynyT <rjoytilley@gmail.com>
2021-05-06 02:34:25 -0700

    forgot one file

--- server/gateway/models/vaccinetypes/mysqlstore.go
+++ server/gateway/models/vaccinetypes/mysqlstore.go
@@ -2,7 +2,7 @@ package vaccinetypes
 
 import (
 	"database/sql"
-	"time"
+	"strings"
 )
 
 // GetByType is an enumerate for GetBy* functions implemented
@@ -11,8 +11,8 @@ type GetByType string
 
 // These are the enumerates for GetByType
 const (
-	ID GetByType = "DemographicID"
-	UserID GetByType = "UserID"
+	ID GetByType = "VaccineTypeID"
+	Name GetByType = "VaccineTypeName"
 )
 
 // MySQLStore is a user.Store backed by MySQL
@@ -29,4 +29,59 @@ func NewMySQLStore(dataSourceName string) (*MySQLStore, error) {
 	}
 
 	return &MySQLStore{db}, nil
+}
+
+// getByProvidedType gets a specific vaccineType given the provided type.
+// This requires the GetByType to be "unique" in the database - hence VaccineTypeID and VaccineTypeName
+func (ms *MySQLStore) getByProvidedType(t GetByType, arg interface{}) (*VaccineType, error) {
+	sel := string("SELECT VaccineTypeID, RiskCoefficient, VaccineTypeName FROM TblVaccineType WHERE " + t + " = ?")
+
+	rows, err := ms.Database.Query(sel, arg)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	vaccineType := &VaccineType{}
+
+	// Should never have more than one row, so only grab one
+	rows.Next()
+	if err := rows.Scan(
+		&vaccineType.VaccineTypeID,
+		&vaccineType.RiskCoefficient,
+		&vaccineType.VaccineTypeName); err != nil {
+		return nil, err
+	}
+	return vaccineType, nil
+}
+
+//GetByID returns the VaccineType with the given ID
+func (ms *MySQLStore) GetByID(id int64) (*VaccineType, error) {
+	return ms.getByProvidedType(ID, id)
+}
+
+//GetByName returns the VaccineType with the Name
+func (ms *MySQLStore) GetByName(name string) (*VaccineType, error) {
+	return ms.getByProvidedType(Name, name)
+}
+
+// Gets all surveys based on a given:
+// Risk Coefficient
+func (ms *MySQLStore) AllVaccineTypes(rc float64) (*[]VaccineType, error) {
+	sel := string("SELECT VaccineTypeID, RiskCoefficient, VaccineTypeName FROM TblVaccineType WHERE RiskCoefficient = ?")
+
+	rows, err := ms.Database.Query(sel, rc)
+	var vaccineTypes []VaccineType
+
+	for rows.Next() {
+		var vaccineType VaccineType
+		err = rows.Scan(&vaccineType.VaccineTypeID, &vaccineType.RiskCoefficient, &vaccineType.VaccineTypeName)
+		
+		if err != nil {
+			return nil, err
+		}
+		surveys = append(vaccineTypes, vaccineType)
+	}
+
+	return &vaccineTypes, nil
 }
