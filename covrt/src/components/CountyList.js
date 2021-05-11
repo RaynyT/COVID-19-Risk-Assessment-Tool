a1d350d4914804615df5bd635969d8da3998e698
Chris Hogan <chrishogan17@outlook.com>
2021-05-10 23:54:25 -0700

    Add mpty inputs for all 50 states and DC

--- covrt/src/components/CountyList.js
+++ covrt/src/components/CountyList.js
@@ -3,6 +3,261 @@ import { Input } from 'reactstrap'
 export default function CountyList(props) {
 
     let list = {
+        AL: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+                <option>Autauga County</option>
+                <option>Baldwin County</option>
+                <option>Barbour County</option>
+                <option>Bibb County</option>
+                <option>Blount County</option>
+                <option>Bullock County</option>
+                <option>Butler County</option>
+                <option>Calhoun County</option>
+                <option>Chambers County</option>
+                <option>Cherokee County</option>
+                <option>Chilton County</option>
+                <option>Choctaw County</option>
+                <option>Clarke County</option>
+                <option>Clay County</option>
+                <option>Cleburne County</option>
+                <option>Coffee County</option>
+                <option>Colbert County</option>
+                <option>Conecuh County</option>
+                <option>Coosa County</option>
+                <option>Covington County</option>
+                <option>Crenshaw County</option>
+                <option>Cullman County</option>
+                <option>Dale County</option>
+                <option>Dallas County</option>
+                <option>DeKalb County</option>
+                <option>Elmore County</option>
+                <option>Escambia County</option>
+                <option>Etowah County</option>
+                <option>Fayette County</option>
+                <option>Franklin County</option>
+                <option>Geneva County</option>
+                <option>Greene County</option>
+                <option>Hale County</option>
+                <option>Henry County</option>
+                <option>Houston County</option>
+                <option>Jackson County</option>
+                <option>Jefferson County</option>
+                <option>Lamar County</option>
+                <option>Lauderdale County</option>
+                <option>Lawrence County</option>
+                <option>Lee County</option>
+                <option>Limestone County</option>
+                <option>Lowndes County</option>
+                <option>Macon County</option>
+                <option>Madison County</option>
+                <option>Marengo County</option>
+                <option>Marion County</option>
+                <option>Marshall County</option>
+                <option>Mobile County</option>
+                <option>Monroe County</option>
+                <option>Montgomery County</option>
+                <option>Morgan County</option>
+                <option>Perry County</option>
+                <option>Pickens County</option>
+                <option>Pike County</option>
+                <option>Randolph County</option>
+                <option>Russell County</option>
+                <option>St. Clair County</option>
+                <option>Shelby County</option>
+                <option>Sumter County</option>
+                <option>Talladega County</option>
+                <option>Tallapoosa County</option>
+                <option>Tuscaloosa County</option>
+                <option>Walker County</option>
+                <option>Washington County</option>
+                <option>Wilcox County</option>
+                <option>Winston County</option>
+            </Input>
+        ),
+        AK: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        AZ: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        AR: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        CA: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        CO: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        CT: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        DC: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        DE: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        FL: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        GA: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        HI: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        ID: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        IL: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        IN: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        IA: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        KS: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        KY: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        LA: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        ME: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        MD: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        MA: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        MI: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        MN: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        MS: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        MO: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        MT: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        NE: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        NV: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        NH: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        NJ: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        NM: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        NY: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        NC: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        ND: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        OH: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        OK: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        OR: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        PA: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        RI: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        SC: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        SD: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        TN: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        TX: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        UT: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        VT: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        VA: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
         WA: (
             <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
                 <option>Adams</option>
@@ -45,17 +300,29 @@ export default function CountyList(props) {
                 <option>Whitman</option>
                 <option>Yakima</option>
             </Input>
+        ),
+        WV: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        WI: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
+        ),
+        WY: (
+            <Input type="select" name="county" className="w-auto" defaultValue={props.selection}>
+            </Input>
         )
     }
-
+    
     let countyOptions = <h1>ERROR</h1>;
     if (list.hasOwnProperty(props.stateCode)) {
         countyOptions = list[props.stateCode];
     } else {
         countyOptions = list["TEST"];
     }
-
+    
     return (
         {countyOptions}
-    );
-}
+        );
+    }
