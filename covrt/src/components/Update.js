9dc2803c140a06519c9fc2d77443833f7807c81d
Chris Hogan <chrishogan17@outlook.com>
2021-05-16 15:15:47 -0700

    Fix vaccine and location pages

--- covrt/src/components/Update.js
+++ covrt/src/components/Update.js
@@ -41,10 +41,6 @@ export default function Update(props) {
             event.target.county.value
         );
 
-        axios.post('https://covidaware.ischool.uw.edu/retrieve_county_rates', props.userLocation)
-        .then(response => console.log(response))
-        .catch(error => console.log(error));
-
         handleNextClick();
     }
 
@@ -61,13 +57,13 @@ export default function Update(props) {
         // If no vaccine was selected, the form will not have rendered all components, so this check
         // prevents refrencing a null variable
         if (event.target.vaccine.value === "None") {
-            props.updateVaccination({ type: "None", doseNumber: 0, effectiveDoseNumber: 0, twoWeeks: null});
-        }else {
+            props.updateVaccination({ type: "None", doseNumber: 0, effectiveDoseNumber: 0, twoWeeks: null });
+        } else {
             let twoWeeks = event.target.weeks.value;
 
             let doseNumber = 0;
             let effectiveDoseNumber = 1;
-            
+
             // If J&J was selected use defaults, if not check the selections
             if (event.target.vaccine.value !== "J&J") {
                 doseNumber = event.target.doses.value;
@@ -79,7 +75,7 @@ export default function Update(props) {
                 effectiveDoseNumber--;
             }
 
-            props.updateVaccination({ 
+            props.updateVaccination({
                 type: event.target.vaccine.value,
                 doseNumber: doseNumber,
                 effectiveDoseNumber: effectiveDoseNumber,
@@ -91,7 +87,7 @@ export default function Update(props) {
     }
 
     const calculateRiskScore = () => {
-        
+
         const numericValues = {
             "indoors": 1,
             "outdoors": .05,
@@ -108,7 +104,7 @@ export default function Update(props) {
             "noMask": 1
         }
         let percentOthersWearingMask = props.othersMask.numWearers / props.activityBasicInfo.attendees
-        
+
         // Determine vaccine efficacy
         let vaccineEfficacy = 1;
         let doseInt = parseInt(props.vaccination.effectiveDoseNumber);
@@ -121,29 +117,29 @@ export default function Update(props) {
                 vaccineEfficacy = .4;
             }
         }
-        
+
         let score = (
             // Activity setting risk coefficient * Number of attendees
             numericValues[props.activityBasicInfo.setting] * props.activityBasicInfo.attendees *
             // * Duration in hours
             (props.activityBasicInfo.hours + (props.activityBasicInfo.minutes / 60)) *
             // * Own mask type risk coefficent
-            numericValues[props.ownMask] * 
+            numericValues[props.ownMask] *
             // * (Others mask type risk * percent of others wearing mask + (100 - percent of others wearing mask))
             (numericValues[props.othersMask.type] * percentOthersWearingMask + (100 - percentOthersWearingMask)) *
             // * Distancing risk * Volume risk
             numericValues[props.distancing] * numericValues[props.speakingVolume] *
             // * Vaccine efficacy
             vaccineEfficacy
-            );
-        
+        );
+
         return score;
     }
 
     const completeSurvey = () => {
 
         props.updateRiskScore(calculateRiskScore());
-        
+
         let surveyData = {
             userID: props.userID,
             userLocation: props.userLocation,
@@ -158,12 +154,12 @@ export default function Update(props) {
         }
 
         axios.post('https://covidaware.ischool.uw.edu/insert_survey', surveyData)
-        .then(response => console.log(response))
-        .catch(error => console.log(error));
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
 
         axios.post('https://covidaware.ischool.uw.edu/retrieve_county_rates', surveyData)
-        .then(response => console.log(response))
-        .catch(error => console.log(error));
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
 
 
         props.history.push('/dashboard');
@@ -171,37 +167,35 @@ export default function Update(props) {
 
     switch (pageNum) {
         case 1:
-            pageScreen = <LocationPage 
-                nextClickCallback={handleNextClick} 
-                backClickCallback={handleBackClick}
-                stateSelectionCallback={handleStateCodeChange} 
+            pageScreen = <LocationPage
+                nextClickCallback={handleNextClick}
+                stateSelectionCallback={handleStateCodeChange}
                 submitCallback={handleLocationPageSubmit}
-                selection={props.userLocation} 
+                selection={props.userLocation}
             />;
-        break;
+            break;
         case 2:
             pageScreen = <VaccinePage
                 backClickCallback={handleBackClick}
                 submitCallback={handleVaccinePageSubmit}
                 vaccineTypeCallback={handleVaccineTypeChange}
                 selection={props.vaccination}
-             />;
-        break;
+            />;
+            break;
         default:
-            pageScreen = <LocationPage 
-                nextClickCallback={handleNextClick} 
-                backClickCallback={handleBackClick}
-                stateSelectionCallback={handleStateCodeChange} 
+            pageScreen = <LocationPage
+                nextClickCallback={handleNextClick}
+                stateSelectionCallback={handleStateCodeChange}
                 submitCallback={handleLocationPageSubmit}
-                selection={props.userLocation} 
-            />;  
-        }
+                selection={props.userLocation}
+            />;
+    }
 
     return (
         <div className="calc-outer">
             <div className="calc-main-container">
                 {pageScreen}
-            </div>            
+            </div>
         </div>
     );
 }
@@ -217,71 +211,71 @@ function LocationPage(props) {
             <Form id="location-form" onSubmit={props.submitCallback}>
                 <FormGroup tag="fieldset">
                     <Label>State:</Label>
-                        <Input type="select" name="state" className="w-auto" defaultValue={props.selection.stateCode} onChange={props.stateSelectionCallback}>
-                            <option value="AL">AL</option>
-                            <option value="AK">AK</option>
-                            <option value="AZ">AZ</option>
-                            <option value="AR">AR</option>
-                            <option value="CA">CA</option>
-                            <option value="CO">CO</option>
-                            <option value="CT">CT</option>
-                            <option value="DE">DE</option>
-                            <option value="DC">DC</option>
-                            <option value="FL">FL</option>
-                            <option value="GA">GA</option>
-                            <option value="HI">HI</option>
-                            <option value="ID">ID</option>
-                            <option value="IL">IL</option>
-                            <option value="IN">IN</option>
-                            <option value="IA">IA</option>
-                            <option value="KS">KS</option>
-                            <option value="KY">KY</option>
-                            <option value="LA">LA</option>
-                            <option value="ME">ME</option>
-                            <option value="MD">MD</option>
-                            <option value="MA">MA</option>
-                            <option value="MI">MI</option>
-                            <option value="MN">MN</option>
-                            <option value="MS">MS</option>
-                            <option value="MO">MO</option>
-                            <option value="MT">MT</option>
-                            <option value="NE">NE</option>
-                            <option value="NV">NV</option>
-                            <option value="NH">NH</option>
-                            <option value="NJ">NJ</option>
-                            <option value="NM">NM</option>
-                            <option value="NY">NY</option>
-                            <option value="NC">NC</option>
-                            <option value="ND">ND</option>
-                            <option value="OH">OH</option>
-                            <option value="OK">OK</option>
-                            <option value="OR">OR</option>
-                            <option value="PA">PA</option>
-                            <option value="PR">PR</option>
-                            <option value="RI">RI</option>
-                            <option value="SC">SC</option>
-                            <option value="SD">SD</option>
-                            <option value="TN">TN</option>
-                            <option value="TX">TX</option>
-                            <option value="UT">UT</option>
-                            <option value="VT">VT</option>
-                            <option value="VA">VA</option>
-                            <option value="WA">WA</option>
-                            <option value="WV">WV</option>
-                            <option value="WI">WI</option>
-                            <option value="WY">WY</option>
-                        </Input>
+                    <Input type="select" name="state" className="w-auto" defaultValue={props.selection.stateCode} onChange={props.stateSelectionCallback}>
+                        <option value="AL">AL</option>
+                        <option value="AK">AK</option>
+                        <option value="AZ">AZ</option>
+                        <option value="AR">AR</option>
+                        <option value="CA">CA</option>
+                        <option value="CO">CO</option>
+                        <option value="CT">CT</option>
+                        <option value="DE">DE</option>
+                        <option value="DC">DC</option>
+                        <option value="FL">FL</option>
+                        <option value="GA">GA</option>
+                        <option value="HI">HI</option>
+                        <option value="ID">ID</option>
+                        <option value="IL">IL</option>
+                        <option value="IN">IN</option>
+                        <option value="IA">IA</option>
+                        <option value="KS">KS</option>
+                        <option value="KY">KY</option>
+                        <option value="LA">LA</option>
+                        <option value="ME">ME</option>
+                        <option value="MD">MD</option>
+                        <option value="MA">MA</option>
+                        <option value="MI">MI</option>
+                        <option value="MN">MN</option>
+                        <option value="MS">MS</option>
+                        <option value="MO">MO</option>
+                        <option value="MT">MT</option>
+                        <option value="NE">NE</option>
+                        <option value="NV">NV</option>
+                        <option value="NH">NH</option>
+                        <option value="NJ">NJ</option>
+                        <option value="NM">NM</option>
+                        <option value="NY">NY</option>
+                        <option value="NC">NC</option>
+                        <option value="ND">ND</option>
+                        <option value="OH">OH</option>
+                        <option value="OK">OK</option>
+                        <option value="OR">OR</option>
+                        <option value="PA">PA</option>
+                        <option value="PR">PR</option>
+                        <option value="RI">RI</option>
+                        <option value="SC">SC</option>
+                        <option value="SD">SD</option>
+                        <option value="TN">TN</option>
+                        <option value="TX">TX</option>
+                        <option value="UT">UT</option>
+                        <option value="VT">VT</option>
+                        <option value="VA">VA</option>
+                        <option value="WA">WA</option>
+                        <option value="WV">WV</option>
+                        <option value="WI">WI</option>
+                        <option value="WY">WY</option>
+                    </Input>
                 </FormGroup>
                 <FormGroup tag="fieldset">
                     <Label>County:</Label>
-                        <CountyList stateCode={props.selection.stateCode} selection={props.selection.county} />
+                    <CountyList stateCode={props.selection.stateCode} selection={props.selection.county} />
                 </FormGroup>
             </Form>
             <img className="calc-img" src={locationImage} alt="Illustration of two doctors" />
 
             <div className="calc-nav-controls">
                 <div className="prev-next-btns">
-                    <button className="btn prev-btn" onClick={props.backClickCallback} aria-label="Previous step">
+                    <button className="btn prev-btn hidden" onClick={props.backClickCallback} aria-label="Previous step">
                         <ChevronLeftIcon size={48} fill="#4A7CE2" />
                     </button>
                     <button type="submit" form="location-form" className="btn next-btn" aria-label="Next step">
@@ -299,25 +293,25 @@ function VaccinePage(props) {
     let yesChecked = false;
     let noChecked = false;
 
-    if (props.selection.twoWeeks === "Yes") {
+    if (props.selection.twoWeeks === "yes") {
         yesChecked = true;
-    } else if (props.selection.twoWeeks === "No") {
+    } else if (props.selection.twoWeeks === "no") {
         noChecked = true;
     }
 
     // Render dose form only if the user has selected that they have been vaccinated
     let doseForm = <div></div>;
-    if (props.selection.type !== "None") {
+    if (props.selection.type !== "none") {
 
         // Don't render doseNumberInput if user selected J&J (1 dose vaccine)
         let doseNumberInput = <div></div>;
-        if (props.selection.type !== "J&J"){
+        if (props.selection.type !== "johnsonAndJohnson") {
             doseNumberInput = (
                 <FormGroup tag="fieldset">
                     <Label>How many doses have you received? </Label>
                     <Input type="select" name="doses" className="w-auto" defaultValue={props.selection.doseNumber}>
-                        <option>1</option>
-                        <option>2</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
                     </Input>
                 </FormGroup>
             );
@@ -330,14 +324,14 @@ function VaccinePage(props) {
                     <legend>Has it been two weeks since your last dose?</legend>
                     <FormGroup check required>
                         <Label>
-                            <Input required type="radio" name="weeks" value="Yes" defaultChecked={yesChecked}
+                            <Input required type="radio" name="weeks" value="yes" defaultChecked={yesChecked}
                             />{' '}
                     Yes
                 </Label>
                     </FormGroup>
                     <FormGroup check required>
                         <Label>
-                            <Input required type="radio" name="weeks" value="No" defaultChecked={noChecked}
+                            <Input required type="radio" name="weeks" value="no" defaultChecked={noChecked}
                             />{' '}
                     No
                 </Label>
@@ -346,28 +340,27 @@ function VaccinePage(props) {
             </div>
         );
     }
-    
-    
+
     return (
         <div className="calc-step-container">
             <h1 className="calc-step-title">What is your vaccination status?</h1>
-            <h2 className="calc-step-desc">Description placeholder</h2>
+            <h2 className="calc-step-desc">The COVID-19 vaccine drastically reduces your chance of catching and spreading the virus</h2>
             <img className="calc-img" src={doctorsImage} alt="Illustration of two doctors" />
             <Form id="vaccine-form" onSubmit={props.submitCallback}>
                 <FormGroup tag="fieldset">
-                    <Label>Have you recieved a COVID-19 vaccine? If so, which type?</Label>
-                        <Input type="select" name="vaccine" className="w-auto" defaultValue={props.selection.type} onChange={props.vaccineTypeCallback}>
-                            <option value="None">No</option>
-                            <option value="Pfizer">Yes - Pfizer</option>
-                            <option value="Moderna">Yes - Moderna</option>
-                            <option value="J&J">Yes - Johnson & Johnson</option>
-                            <option value="Astrazeneca">Yes - Astrazeneca</option>
-                            <option value="Other">Yes - Other</option>
-                        </Input>
+                    <Label>Have you received a COVID-19 vaccine? If so, which type?</Label>
+                    <Input type="select" name="vaccine" className="w-auto" defaultValue={props.selection.type} onChange={props.vaccineTypeCallback}>
+                        <option value="none">No</option>
+                        <option value="pfizer">Yes - Pfizer</option>
+                        <option value="moderna">Yes - Moderna</option>
+                        <option value="johnsonAndJohnson">Yes - Johnson & Johnson</option>
+                        <option value="astrazeneca">Yes - Astrazeneca</option>
+                        <option value="other">Yes - Other</option>
+                    </Input>
                 </FormGroup>
                 {doseForm}
             </Form>
-            
+
             <div className="calc-nav-controls">
                 <div className="prev-next-btns">
                     <button className="btn prev-btn" onClick={props.backClickCallback} aria-label="Previous step">
@@ -381,7 +374,7 @@ function VaccinePage(props) {
                     </button>
                 </div>
             </div>
-            
+
         </div>
     );
 }
