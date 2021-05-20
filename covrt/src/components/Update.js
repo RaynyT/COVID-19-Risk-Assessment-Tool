842850f57f690ff21ec7be7875b068526c88e5e7
Chris Hogan <chrishogan17@outlook.com>
2021-05-20 14:44:37 -0700

    Use calculation function

--- covrt/src/components/Update.js
+++ covrt/src/components/Update.js
@@ -4,6 +4,8 @@ import { ChevronLeftIcon, ChevronRightIcon } from '@primer/octicons-react';
 import axios from 'axios';
 import moment from 'moment';
 import CountyList from './CountyList';
+import calculateRiskScore from './CalculateRiskScore.js';
+
 
 
 
@@ -114,61 +116,11 @@ export default function Update(props) {
         completeSurvey();
     }
 
-    const calculateRiskScore = () => {
-
-        const numericValues = {
-            "indoors": 1,
-            "outdoors": .05,
-            "lessThanSixFeet": 1,
-            "sixFeet": .5,
-            "nineFeet": .25,
-            "moreThanNineFeet": .125,
-            "notSpeaking": .20,
-            "normalSpeaking": 1,
-            "loudSpeaking": 5,
-            "cottonMask": .6666666666,
-            "surgicalMask": .5,
-            "kn95Mask": .3333333333,
-            "noMask": 1
-        }
-        let percentOthersWearingMask = props.othersMask.numWearers / props.activityBasicInfo.attendees
-
-        // Determine vaccine efficacy
-        let vaccineEfficacy = 1;
-        let doseInt = parseInt(props.vaccination.effectiveDoseNumber);
-        if (doseInt === 1) {
-            vaccineEfficacy = .56;
-        } else if (doseInt === 2) {
-            if (props.vaccination.type === "pfizer" || props.vaccination.type === "moderna") {
-                vaccineEfficacy = .1;
-            } else {
-                vaccineEfficacy = .4;
-            }
-        }
-
-        let score = (
-            // Activity setting risk coefficient * Number of attendees
-            numericValues[props.activityBasicInfo.setting] * props.activityBasicInfo.attendees *
-            // * Duration in hours
-            (props.activityBasicInfo.hours + (props.activityBasicInfo.minutes / 60)) *
-            // * Own mask type risk coefficent
-            numericValues[props.ownMask] *
-            // * (Others mask type risk * percent of others wearing mask + (100 - percent of others wearing mask))
-            (numericValues[props.othersMask.type] * percentOthersWearingMask + (1 - percentOthersWearingMask)) *
-            // * Distancing risk * Volume risk
-            numericValues[props.distancing] * numericValues[props.speakingVolume] *
-            // * Vaccine efficacy
-            vaccineEfficacy *
-            // * Person Risk
-            props.personRisk
-        );
-
-        return score;
-    }
-
     const completeSurvey = () => {
 
-        props.updateRiskScore(calculateRiskScore());
+        let riskScore = calculateRiskScore({...props});
+        console.log(riskScore);
+        props.updateRiskScore(riskScore);
 
         let surveyData = {
             userID: props.userID,
@@ -179,7 +131,7 @@ export default function Update(props) {
             speakingVolume: props.speakingVolume,
             ownMask: props.ownMask,
             othersMask: props.othersMask,
-            riskScore: props.riskScore,
+            riskScore: riskScore,
             surveyCompleted: props.surveyCompleted,
         }
 
